57e436ce7fef59b98813e29b40903c1ac89e0e9c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-05 14:17:04 +0600

    button centered

--- app/page.js
+++ app/page.js
@@ -12,11 +12,21 @@ export default function Home() {
 
   return (
     <div>
-      <button onClick={handleStartClick}>Start Countdown</button>
-      <Card
-        setStartCountdown={setStartCountdown}
-        startCountdown={startCountdown}
-      />
+      {startCountdown ? (
+        <Card
+          setStartCountdown={setStartCountdown}
+          startCountdown={startCountdown}
+        />
+      ) : (
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-green-500 text-lg font-bold mt-14 px-5 py-2 rounded-lg"
+            onClick={handleStartClick}
+          >
+            Start Countdown &#8594;
+          </button>
+        </div>
+      )}
     </div>
   );
 }
