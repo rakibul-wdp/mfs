ec68f7c297f5a0be8a96db325964b6b0246e7a9d
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-18 05:31:54 +0600

    configure auth0

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const indexRouter = require("./routes/index.js");
+const { auth } = require("express-openid-connect");
+require("dotenv").config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
 
 const app = express();
 app.set("views", "views");
@@ -7,6 +18,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(auth(config));
 
 app.use("/", indexRouter);
 
