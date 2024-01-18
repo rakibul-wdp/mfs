d220704284a4d399bf1b539d97fdc1458fc0356f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-18 09:19:24 +0600

    add some more styles

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { requiresAuth } = require("express-openid-connect");
-const axios = require("axios");
 
 router.get("/", (req, res) => {
   res.render("index", {
@@ -11,19 +10,11 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/secured", requiresAuth(), async (req, res) => {
-  let data = {};
-
-  try {
-    const apiResponse = await axios.get("http://localhost:5000/public");
-    data = apiResponse.data;
-  } catch (error) {}
-
+router.get("/secured", requiresAuth(), (req, res) => {
   res.render("secured", {
     title: "Secure Page",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
-    data,
   });
 });
 
