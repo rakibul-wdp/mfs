a4dfce0fd060f218c29af28637d809f383360f49
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-18 07:33:50 +0600

    create secured and common nav views

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req.oidc.isAuthenticated());
   res.render("index", {
     title: "Express Demo",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+  });
+});
+
+router.get("/secured", (req, res) => {
+  res.render("secured", {
+    title: "Secure Page",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
   });
